c50181060c96ffe47228ed6169aacaa064f0e427
TJ Saunders <tj@castaglia.org>
2016-12-06 14:58:32 -0800

    Stub out the API that we'll use to supply the name/ID mappings.

---
+++ names.c
@@ -0,0 +1,39 @@
+/* 
+ * ProFTPD - mod_rsync name/ID mapping
+ * Copyright (c) 2010-2016 TJ Saunders
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * As a special exemption, TJ Saunders and other respective copyright holders
+ * give permission to link this program with OpenSSL, and distribute the
+ * resulting executable, without including the source code for OpenSSL in the
+ * source distribution.
+ */
+
+#include "mod_rsync.h"
+#include "session.h"
+#include "names.h"
+
+static const char *trace_channel = "rsync.names";
+
+const char *rsync_names_add_uid(struct rsync_session *sess, uid_t uid) {
+  errno = ENOSYS;
+  return NULL;
+}
+
+const char *rsync_names_add_gid(struct rsync_session *sess, gid_t gid) {
+  errno = ENOSYS;
+  return NULL;
+}
---
+++ names.h
@@ -0,0 +1,34 @@
+/*
+ * ProFTPD - mod_rsync name/ID mappings
+ * Copyright (c) 2016 TJ Saunders
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * As a special exemption, TJ Saunders and other respective copyright holders
+ * give permission to link this program with OpenSSL, and distribute the
+ * resulting executable, without including the source code for OpenSSL in the
+ * source distribution.
+ */
+
+#ifndef MOD_RSYNC_NAMES_H
+#define MOD_RSYNC_NAMES_H
+
+#include "mod_rsync.h"
+#include "session.h"
+
+const char *rsync_names_add_uid(struct rsync_session *sess, uid_t uid);
+const char *rsync_names_add_gid(struct rsync_session *sess, gid_t gid);
+
+#endif /* MOD_RSYNC_NAMES_H */
